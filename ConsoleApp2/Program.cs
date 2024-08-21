0d07e81088264bd3ce0a0045b9eb607e2eaa10b3
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2024-08-21 21:00:10 +0700

    update: learn about class and inheritance

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,7 +2,6 @@
 
 namespace MyNamespace
 {
-   
 
     class Program
     {
@@ -16,37 +15,37 @@ namespace MyNamespace
             // Mybio mybioData = new Mybio("fariz", 21, "male");
             // Console.WriteLine(mybioData);
             // make a standard clalculator 
-            double num1, num2, result;
-            char op;
-            Console.Write("masukkan angka: ");
-            num1 = double.Parse(Console.ReadLine());
-            Console.Write("enter operation (+,-,x,/) :");
-            op = char.Parse(Console.ReadLine());
-            Console.Write("enter num :");
-            num2 = double.Parse(Console.ReadLine());
+            // double num1, num2, result;
+            // char op;
+            // Console.Write("masukkan angka: ");
+            // num1 = double.Parse(Console.ReadLine());
+            // Console.Write("enter operation (+,-,x,/) :");
+            // op = char.Parse(Console.ReadLine());
+            // Console.Write("enter num :");
+            // num2 = double.Parse(Console.ReadLine());
             
-            switch (op)
-            {
-                case '+':
-                    result = num1 + num2;
-                    Console.WriteLine(num1 + "+" + num2 + '=' + result);
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    Console.WriteLine(num1 + "-" + num2 + '=' + result);
-                    break;
-                case 'x':
-                    result = num1 * num2;
-                    Console.WriteLine(num1 + "x" + num2 + '=' + result);
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    Console.WriteLine(num1 + "/" + num2 + '=' + result);
-                    break;
-                default:
-                    Console.WriteLine("Unknown operation!");
-                    break;
-            }
+            // switch (op)
+            // {
+            //     case '+':
+            //         result = num1 + num2;
+            //         Console.WriteLine(num1 + "+" + num2 + '=' + result);
+            //         break;
+            //     case '-':
+            //         result = num1 - num2;
+            //         Console.WriteLine(num1 + "-" + num2 + '=' + result);
+            //         break;
+            //     case 'x':
+            //         result = num1 * num2;
+            //         Console.WriteLine(num1 + "x" + num2 + '=' + result);
+            //         break;
+            //     case '/':
+            //         result = num1 / num2;
+            //         Console.WriteLine(num1 + "/" + num2 + '=' + result);
+            //         break;
+            //     default:
+            //         Console.WriteLine("Unknown operation!");
+            //         break;
+            // }
 
             int noCar = 12;
             Console.WriteLine(Convert.ToString(noCar));
@@ -60,6 +59,53 @@ namespace MyNamespace
             int indexNamaBelakang = myOshi.IndexOf("y");
             string namaBelakang = myOshi.Substring(indexNamaBelakang);
             Console.WriteLine($"nama belakang dari {myOshi} adalah {namaBelakang}");
+            bool isAdmin = true;
+            bool isStudent = true;
+            if (isAdmin && isStudent)
+            {
+                Console.WriteLine("you are a admin and student");
+            }else if (!isAdmin || isStudent)
+            {
+                Console.WriteLine("you are a student");
+            } else if (!isStudent)
+            {
+                Console.WriteLine("you are not a student");
+            }
+            else
+            {
+                Console.WriteLine("you are not a admin");
+            }
+            int []number_list = {1,2,3,4,5,6,7,8,9,10};
+            int largestNumber = number_list[0];
+            for (int i = 1; i < number_list.Length; i++)
+            {
+                if (number_list[i] > largestNumber)
+                {
+                    largestNumber = number_list[i];
+                }
+            }
+            Console.WriteLine($"the largest numberis a {largestNumber}");
+            Kucing kucingku = new Kucing();
+            kucingku.Mengeong();
+            kucingku.Makan();
         }
     }
+
+    class Hewan
+    {
+        public void Makan()
+        {
+            Console.WriteLine("Makan");
+        }
+    }
+
+    class Kucing : Hewan
+    {
+        public void Mengeong()
+        {
+            Console.WriteLine("Mengeong");
+        }
+ 
+    }
+    
 }
--- ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd460d7bd43c2f38ef9b1f72497567726cd9a1db")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
