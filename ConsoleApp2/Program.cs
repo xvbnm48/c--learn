346dc1c500bad1ab4ef8db10821b8f18358d8912
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2024-08-21 10:34:33 +0700

    init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using MyNamespace.model;
+
+namespace MyNamespace
+{
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Coords coords = new Coords(2, 3);
+            // // Do something with the coords
+            // Console.WriteLine(coords);
+            // var name = "fariz";
+            // Console.WriteLine(name);
+            // Mybio mybioData = new Mybio("fariz", 21, "male");
+            // Console.WriteLine(mybioData);
+            // make a standard clalculator 
+            double num1, num2, result;
+            char op;
+            Console.Write("masukkan angka: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("enter operation (+,-,x,/) :");
+            op = char.Parse(Console.ReadLine());
+            Console.Write("enter num :");
+            num2 = double.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine(num1 + "+" + num2 + '=' + result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine(num1 + "-" + num2 + '=' + result);
+                    break;
+                case 'x':
+                    result = num1 * num2;
+                    Console.WriteLine(num1 + "x" + num2 + '=' + result);
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    Console.WriteLine(num1 + "/" + num2 + '=' + result);
+                    break;
+                default:
+                    Console.WriteLine("Unknown operation!");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp2/model/model.cs
@@ -0,0 +1,30 @@
+﻿namespace MyNamespace.model;
+
+public struct Mybio
+{
+    public string Name;
+    public int Age;
+    public string Gender;
+    public Mybio(string name, int ages, string gender)
+    {
+        Name = name;
+        Age = ages;
+        Gender = gender;
+    }
+
+    public override string ToString() => $"{Name} {Age} {Gender}";
+}
+
+public struct Coords
+{
+    public Coords(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public double X { get; }
+    public double Y { get; }
+
+    public override string ToString() => $"({X}, {Y})";
+}
